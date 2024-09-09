b5e32b9c23f81bb46883d6797301b9a978b4a08a
Noman Atique <noman.atique@keyin.com>
2024-09-09 08:41:52 -0400

    september9

---
+++ JavaScript/Week2/scripts/script.js
@@ -0,0 +1,54 @@
+// // console.log("Welcome to week2");
+// // console.log("Hope you understand");
+
+// // // semi colon is called "Statement Terminator"
+
+// // // customerCount IS NOT SAME AS  CustomerCount
+// // // Both are considered different...
+// // /*
+// // Thios is multiline comment
+// // */
+
+// // if (x > 10) {
+// //   // body of if statment
+// // } else {
+// //   // body of else...
+// // }
+
+// // let name = "Noman Atique";
+// // let name1 = "Noman Atique";
+
+// // let x;
+// // x = 10;
+// // x = "Noman";
+
+// // var y;
+// // const z = 10;
+
+// // // int x; IN C-FAMILY
+
+// // let t = 10;
+
+// // let x911 = 10;
+
+// // let y = 14;
+// // let z = y%3;
+
+// let x = 10;
+
+// let y = x + 10;
+// //  y+=10;
+// Increment/Decrement:
+
+// ++ , --
+
+// Pre-increment & Post-increment
+
+let x = 10;
+console.log(x++); //internally x will become 11
+console.log(x); // output will be
+console.log(--x); //??
+console.log(x--); //??
+console.log(x);
+
+console.log("END OF CLASS!");
