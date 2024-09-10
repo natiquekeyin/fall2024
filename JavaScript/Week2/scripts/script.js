57fe17ae71a340fbb60db601212f136559dd6686
Noman Atique <noman.atique@keyin.com>
2024-09-10 12:34:33 -0400

    sep10

--- JavaScript/Week2/scripts/script.js
+++ JavaScript/Week2/scripts/script.js
@@ -44,11 +44,84 @@
 
 // Pre-increment & Post-increment
 
-let x = 10;
-console.log(x++); //internally x will become 11
-console.log(x); // output will be
-console.log(--x); //??
-console.log(x--); //??
-console.log(x);
+// let x = 10;
+// console.log(x++); //internally x will become 11
+// console.log(x); // output will be
+// console.log(--x); //??
+// console.log(x--); //??
+// console.log(x);
 
-console.log("END OF CLASS!");
+// Lists in Python... Arrys in JavaScript!..
+
+// let x = [1, 2, 3];
+// let i = 0;
+
+// console.log(x[i++]); //1
+// console.log(x[++i]); //2
+
+// Stict equal operator??
+
+// Coercion:automatic conversion of data type in comparison..
+
+// Strict equality operator : ===  ... to avoid "coercion"
+
+// let x = 10;
+// if (x === "10") {
+//   console.log("yes");
+// } else {
+//   console.log("No");
+// }
+
+// let x;
+// x = 10;
+// console.log(typeof x);
+// x = "Noman";
+// console.log(typeof x);
+
+// Binary Operators: + , -  , * , / ... THEY REQUIRE TWO OPERANDS... X+
+// Uniary Operators: ++, -- , !true, !x ... x!=4
+
+// Ternary Operator: is an operator that requires 3 operands...
+
+//   ?:
+
+// syntax:        condition ? Value_if_true:value_if_false;
+
+// let age = 10;
+// let message;
+
+// // age > 18 ? console.log("Adult") : console.log("Child");
+// // It is a short form of if else... BUT some places in REACT.. it is ONLY allowed and we can't use if else there...
+
+// message = age > 18 ? "Adult" : "Child";
+// console.log(message);
+
+// let name1 = "Alan SMith";
+// let age = 20;
+
+// console.log("Welcome " + name1 + ". Your age is " + age);
+
+// template literals
+
+// console.log(`Welcome ${name1[0].toTitleCase()}. Your age is ${age + 20}`);
+
+// Try to always use backticks...
+
+// let name1 = "Alan SMith";
+// let age = 20;
+// console.log("hello");
+// console.log("something");
+// age > 18 ? console.log("Adult") : console.log("Child");
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`(${i},${j})`);
+//   }
+// }
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < i; j++) {
+    console.log(`${i},${j}`);
+  }
+  console.log("\n");
+}
