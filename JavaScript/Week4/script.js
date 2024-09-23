fae9d3f214b42279ed26857d176a026d2caa3c63
Noman Atique <noman.atique@keyin.com>
2024-09-23 12:21:25 -0400

    week4_JS

---
+++ JavaScript/Week4/script.js
@@ -0,0 +1,43 @@
+// for loop:
+// for (let i = 0; i < nums.length; i++) {
+//   console.log(nums[i] + 3);
+// }
+
+//
+
+// let nums = [3, 2, 6, 7];
+
+// let listCopy = [];
+// nums.forEach(function (element) {
+//   listCopy.push(element + 3);
+// });
+
+// let nums1 = [3, 2, 6, 7];
+
+// let listCopy1 = nums.map((element) => element + 3);
+// console.log(listCopy1);
+
+let nums = [2, 5, 1, 6, 7];
+// filter() filters out values on the basis of given condition inside the callback function
+
+// let numsCopy = nums.filter((value) => value > 3);
+// let numsCopy2 = nums.map((value) => value > 3);
+
+// let numsCopy = [];
+// nums.forEach((value) => {
+//   if (value > 3) {
+//     numsCopy.push(value);
+//   }
+// });
+
+// console.log(numsCopy);
+// // console.log(numsCopy2);
+
+// Regular Expression...
+//  .  any letter...
+//  [ ] set of leters
+// \d any digit
+//  \w  - word [ a-zA-Z0-9_]
+// *  - 0 or more occurences..
+// +  - 1 or more
+//   \(?\d{3}\)?[\s-]*\d{3}[\s-]*\d{4}
