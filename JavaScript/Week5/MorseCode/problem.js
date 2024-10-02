e6ebade48881f6cfd08fdf39fea47675dbeac151
Noman Atique <noman.atique@keyin.com>
2024-10-02 12:21:24 -0400

    morse

--- JavaScript/Week5/MorseCode/problem.js
+++ JavaScript/Week5/MorseCode/problem.js
@@ -1,23 +1,105 @@
 // Object to provide lookup of morse code (value) for a given letter (key).
 let alpha = {
   // define the mapping here as a literal
+
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  " ": "/",
 };
 
 // Object to provide lookup of letter (value) for a given morse code (key).
-let morse = {};
+let morse = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "/": " ",
+};
 // Hint: use the [] operator to specify these special key values rather than a literal.
 
 // Return `true` if all characters are morse code.  Use a RegExp.
-function isMorse(characters) {}
+function isMorse(characters) {
+  return /^[.\-/]*$/.test(characters);
+}
 
 // Return `true` if all characters are part of the alphabet defined in `alpha`.  Use a RegExp.
 // Bonus: can you rewrite it using `Object.keys()` and your `alpha` Object instead?
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-function isAlpha(characters) {}
+function isAlpha(characters) {
+  return /^[A-Z ]*/.test(characters);
+}
 
 // Given an alphabet message, convert and return in morse code.  Use your morse and/or alpha object.
 // Return undefined if text is not alpha.
-function textToMorse(text) {}
+function textToMorse(text) {
+  function convertWordToMorse(word) {
+    let morse = "";
+    let letter;
+    // Loop acrsos all the letters in the word, convert to morse, and build a new string
+    for (letter of word) {
+      morse += alpha[letter] + " ";
+    }
+    return morse.trim();
+  }
+
+  //   turn text into Upper case (if not already )
+
+  text = text.toUpperCase();
+  //   split into separate words (breaks or spaces)
+  words = text.split(" ");
+  //   Run each word throuh convertWordToMorse() and build a new array
+
+  morse = words.map((word) => convertWordToMorse(word));
+
+  //   return a string of all words converted to morse, joined by /
+  return morse.join("/");
+}
 
 // Given a morse code message, convert and return in text.  Use your morse and/or alpha object.
 // Return undefined if morse is not proper code.
@@ -44,3 +126,6 @@ console.log(msg1.toMorse());
 let msg2 = new Message("I am learning how to use Objects in JavaScript");
 console.log(msg2.toMorse());
 console.log(msg2.toAlpha());
+
+// Let's extend our Thursday's front end session and solve this problem...
+// I will announce a session on Thursday after our regular session...
