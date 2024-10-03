c9b46b2a3111e287193a7ba5c289a8e530458484
Noman Atique <noman.atique@keyin.com>
2024-10-03 11:50:26 -0400

    oct3

--- FrontEnd/Week5/scripts/domevents.js
+++ FrontEnd/Week5/scripts/domevents.js
@@ -2,8 +2,15 @@ window.addEventListener("DOMContentLoaded", function () {
   console.log("DOM Content loaded");
 
   this.document.body.addEventListener("click", function (e) {
-    console.log("Body clicked");
-    console.log(e.target);
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+
+    document.querySelector(
+      "#posX"
+    ).innerHTML = `X position is <strong> ${e.clientX} </strong>and you clicked on ${e.currentTarget} element`;
+    document.querySelector(
+      "#posY"
+    ).innerHTML = `Y position is <strong> ${e.clientY} </strong>and you clicked on ${e.target} element`;
   });
 
   let cat = document.querySelector("#cat");
@@ -12,12 +19,34 @@ window.addEventListener("DOMContentLoaded", function () {
 
   let currentCatIndex = 0;
   function nextCatImageUrl() {
-    return;
+    if (currentCatIndex === cats.length - 1) {
+      currentCatIndex = 0;
+    }
+    return cats[currentCatIndex++];
   }
 
   function handleClick(evt) {
     cat.src = nextCatImageUrl();
 
+    evt.stopPropagation();
+  }
+
+  let btn1 = document.querySelector("#btn1");
+
+  btn1.addEventListener("click", moreCats);
+
+  function moreCats(evt) {
+    let ol = document.createElement("ol");
+
+    cats.forEach((url) => {
+      let li = document.createElement("li");
+      li.innerHTML = `<a href ="${url}">${url}</a>`;
+      ol.appendChild(li);
+    });
+
+    document.querySelector("#list").appendChild(ol);
+
+    // console.log(ol);
     evt.stopPropagation();
   }
 });
