f88157a8611d8337535e55555938607b6c14d6e5
Noman Atique <noman.atique@keyin.com>
2024-11-04 12:28:56 -0500

    reactComponents

---
+++ JavaScript/Week10/firstapp/src/components/Test.jsx
@@ -0,0 +1,10 @@
+function Test() {
+  return (
+    <>
+      <h2>This is Test Component</h2>
+      <p>This is the text in our Test component</p>
+    </>
+  );
+}
+
+export default Test;
---
+++ JavaScript/Week10/firstapp/src/components/Test2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Test2() {
+  return (
+    <>
+      <div>
+        <h1>Test2</h1>
+        <p>Tis is paragraph</p>
+      </div>
+    </>
+  );
+}
+
+export default Test2;
