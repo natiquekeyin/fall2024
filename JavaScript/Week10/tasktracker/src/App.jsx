480f93acccfc006f21be9b7d61a57e047f67485a
Noman Atique <noman.atique@keyin.com>
2024-11-13 08:29:42 -0500

    addTask

--- JavaScript/Week10/tasktracker/src/App.jsx
+++ JavaScript/Week10/tasktracker/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -31,6 +32,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- JavaScript/Week10/tasktracker/src/components/AddTask.jsx
+++ JavaScript/Week10/tasktracker/src/components/AddTask.jsx
@@ -1,9 +1,19 @@
 const AddTask = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor=""></label>
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="Add Task" />
       </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input type="text" placeholder="Add day & time" />
+      </div>
+      <div className="form-control-check">
+        <label>Set Reminder</label>
+        <input type="checkbox" />
+      </div>
+      <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
   );
 };
